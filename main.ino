e259211acdee7f18d7b67cb52bb60cea0e746144
unknown <chrisvill.2312@gmail.com>
2022-08-09 15:54:29 -0300

    modified response as a JSON to be captured by a node.js server and code refactor

--- main.ino
+++ main.ino
@@ -1,62 +1,48 @@
+//------ Ammeter ---------------------------------------------------------------
 #include "ACS712.h"
-
 // Arduino UNO has 5.0 volt with a max ADC value of 1023 steps
 // ACS712 5A  uses 185 mV per A
 // ACS712 20A uses 100 mV per A
 // ACS712 30A uses  66 mV per A
-
-ACS712 ACS(A1, 5.0, 1023, 66);
-const float ACS_ADJUST = 0.9;
 // ESP 32 example (requires resistors to step down the logic voltage)
 // ACS712  ACS(25, 5.0, 4095, 185);
-const float V_ADJUST = 1.005;
+ACS712 ACS(A1, 5.0, 1023, 66);
+const float ACS_ADJUST = 0.9;
+//------ Voltmeter --------------------------------------------------------------
 const float V1 = 4.95;
 const float R1 = 996500; // 1M
 const float R2 = 99200;  // 100K
-const int MODE_PIN = 2;
 const int VOLTAGE_PIN = A0;
-int ledState = LOW;
+const float V_ADJUST = 1.005;
+//------ Constants and States ---------------------------------------------------
+const int MODE_PIN = 2;
 const float ABSOLUTE = 1.0;
 const float TOLERANCE = (ABSOLUTE + .5);
 const int RESOLUTION = 100;
-const bool DENOISE = true;
+const String DENOISE = "DENOISE";
+const String MEASURE = "MEASURE";
 float voltageNoise = 0;
 float ampereNoise = 0;
+int ledState = LOW;
 
 void blink()
 {
   if (ledState == LOW)
-  {
     ledState = HIGH;
-  }
   else
-  {
     ledState = LOW;
-  }
   digitalWrite(LED_BUILTIN, ledState);
 }
 
-float maxVoltage()
-{
-  return V1 / (R2 / (R1 + R2));
-}
+float maxVoltage() { return V1 / (R2 / (R1 + R2)); }
 
-float volts()
-{
-  return (analogRead(VOLTAGE_PIN) * (V1 / 1023.0)) / (R2 / (R1 + R2));
-}
+float volts() { return (analogRead(VOLTAGE_PIN) * (V1 / 1023.0)) / (R2 / (R1 + R2)); }
 
-float getVoltage()
-{
-  return volts() * V_ADJUST;
-}
+float getVoltage() { return volts() * V_ADJUST; }
 
-float getAmperage()
-{
-  return ACS.mA_DC() * ACS_ADJUST;
-}
+float getAmperage() { return ACS.mA_DC() * ACS_ADJUST; }
 
-String getMeasurement(int resolution, bool denoise)
+String getMeasurement(int resolution, String mode)
 {
   String message = "";
   float avgV = 0;
@@ -70,29 +56,26 @@ String getMeasurement(int resolution, bool denoise)
   }
   avgV /= resolution;
   avgA /= resolution;
-  if (denoise)
+  if (mode == DENOISE)
   {
     if (avgV > voltageNoise)
       voltageNoise = avgV * TOLERANCE;
     if (avgA > ampereNoise)
       ampereNoise = avgA * TOLERANCE;
-    message = "DN,";
   }
   else
   {
-    message = "MS,";
     if (avgV < voltageNoise)
       avgV = 0.0;
     if (avgA < ampereNoise)
       avgA = 0.0;
   }
-  message += String(avgV, 2) + "," + String(avgA, 2) + "," + String(((avgV * avgA) / 1000), 2) + "," + String((voltageNoise * ampereNoise) / 1000, 2);
-  return message;
+  // This message was thought to be a JSON message to be captured by a node.js server
+  return String("{\"mode\":\"" + mode + "\",\"voltage\":" + String(avgV, 2) + ",\"mAmps\":" + String(avgA, 2) + ",\"watts\":" + String(((avgV * avgA) / 1000), 2) + ",\"noiseWatts\":" + String((voltageNoise * ampereNoise) / 1000, 2) + "}");
 }
 
 void setup()
 {
-  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MODE_PIN, INPUT);
   ACS.autoMidPoint();
@@ -104,6 +87,5 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Serial.println(getMeasurement(RESOLUTION, digitalRead(MODE_PIN)));
+  Serial.println(getMeasurement(RESOLUTION, digitalRead(MODE_PIN) ? DENOISE : MEASURE));
 }
